3747bae6b34af91015f279cf70cdb4b1759c36c5
Isaque-NR <isaque711.nascimento@gmail.com>
2026-02-02 20:41:49 -0300

    exercicio 4 refeito e refinalizado pq o octave n presta e tive q fazer do zero

--- lista4_ex_4.m
+++ lista4_ex_4.m
@@ -3,51 +3,52 @@ function lista4_ex_4 ()
   x0 = [2.5 ; 2.5];
   maxItr = 1000;
   tol = 1e-3;
-  aN = 1; %  0,0001 <= alpha <= 10
-  aG = 0.0001; % alphas diferentes pois o metodo do gradiente nao estava convergindo com alpha = 1, estava dando NaN
+  aN = 1; %  0,0001 <= alpha <= 10 (nao sei pq alpha maior que 3 quebra o grafico 3d
+  aG = 0.01; % alphas diferentes pois o metodo do gradiente nao estava convergindo com alpha = 1, estava dando NaN
 
+  %chamada dos metodos
   [pontoOtimoN,iN,todosXN,todosYN] = metodoNewton(x0,aN,maxItr,tol);
-  %prints...
-  [pontoOtimoG,iG,todosXG,todosYG] = metodoGradiente(x0,aG,maxItr,tol); % notase que o metodo nao
-   %prints...
+  printarInfo('Newthon',pontoOtimoN,todosXN,todosYN,iN,aN);
+  [pontoOtimoG,iG,todosXG,todosYG] = metodoGradiente(x0,aG,maxItr,tol); % nota se que o metodo nao chegou ao ponto otimo previsto (3,2)
+  printarInfo('Gradiente',pontoOtimoG,todosXG,todosYG,iG,aG);
+   %chamadas para as funcoes de graficos
    [todosZ,intervaloX,intervaloY] = superficie();
-   plotagrafico3DNewton(pontoOtimoN,todosXN,todosYN,todosZ,intervaloX,intervaloY,iN,aN);
+   plotagrafico3D(1,'Newthon',pontoOtimoN,todosXN,todosYN,todosZ,intervaloX,intervaloY,iN,aN);
+   pause(1); % pausa pra acabar a animacao do primiero grafico / ou um keyboard
+   plotagrafico3D(2,'Gradiente',pontoOtimoG,todosXG,todosYG,todosZ,intervaloX,intervaloY,iG,aG);
+   pause(1);
+   plotaconvergenciaX(3,'Newthon',todosXN);
+   pause(1);
+   plotaconvergenciaX(4,'Gradiente',todosXG); % nao chega em (3,2) mas esta convergindo bom sinal
+   pause(1);
+   plotaconvergenciaY(5,'Newthon',todosYN);
+   pause(1);
+   plotaconvergenciaY(6,'Gradiente',todosYG); %converngindo pra zero
+   pause(1);
 
 
 function vetY = f(vetx)
-
   x = vetx(1);
   y = vetx(2);
-
   vetY = (x.^2 + y - 11)^2 + (x + y.^2 - 7)^2;
-
-
 endfunction
 
 function gradiente = gf(vetx) %gradiente de f(x)
-
   x = vetx(1);
   y = vetx(2);
-
   gradiente = nan(2,1);
   gradiente(1) = 4*x^3 + 2*y^2 + 4*x*y - 42*x - 14 ; %vetor coluna com as derivadas parciais
   gradiente(2) = 4*y^3 + 2*x^2 + 4*x*y - 26*y -22 ;
-
-
 endfunction
 
 function haussiana = H(vetx)
-
   x = vetx(1);
   y = vetx(2);
-
   haussiana = nan(2,2);
-
   haussiana(1,1) = 12*x^2 + 4*y - 42;
   haussiana(1,2) = 4*x + 4*y;
   haussiana(2,1) = 4*y + 4*x;
   haussiana(2,2) = 12*y^2 + 4*x -26;
-
 endfunction
 
 function [ pontoOtimoN, iN, todosXN, todosYN] = metodoNewton(x0,a,maxItr,tol)
@@ -112,8 +113,9 @@ function [todosZ,intervaloX,intervaloY] = superficie() %funcao para preencher a 
 
 endfunction
 
-function plotagrafico3DNewton(pontoOtimoN,todosXN,todosYN,todosZ,intervaloX,intervaloY,iN,aN)
-  figure(1);
+function plotagrafico3D(fig,metodo,pontoOtimo,todosX,todosY,todosZ,intervaloX,intervaloY,i,a)
+  figure(fig); % na chamada passa um valor pra criar a figura e nao sobrescrever os graficos entre si
+  n = size(todosX,1); %pegando a quantidade de dados
   clf;
   surf(intervaloX,intervaloY,todosZ); %plotando o grafico com a superficie
   hold on;
@@ -121,47 +123,42 @@ function plotagrafico3DNewton(pontoOtimoN,todosXN,todosYN,todosZ,intervaloX,inte
   xlabel('x');
   ylabel('y');
   zlabel('f(x,y)');
-  title(sprintf('Grafico 3D animado da otimizacao da funcao Himmelblau \nutilizando o metodo de Newthon com alpha = %.6f\nNo ponto inicial (%.6f , %.6f)\n O ponto otimo encontrado foi: (%.6f, %.6f)',aN,todosXN(1,1),todosXN(1,2),pontoOtimoN(1),pontoOtimoN(2)));
+  title(sprintf('Grafico 3D animado da otimizacao da funcao Himmelblau \nutilizando o metodo de %s com alpha = %.6f\nNo ponto inicial (%.6f , %.6f)\n O ponto otimo encontrado foi: (%.6f, %.6f) em %d iteracoes',metodo,a,todosX(1,1),todosX(1,2),pontoOtimo(1),pontoOtimo(2),i));
   colormap jet;
-  plot3(todosXN(:,1),todosXN(:,2),todosYN,'--','linewidth',3,'color',[1 0 1]);
-  ponto = plot3(todosXN(1,1),todosXN(1,2),todosYN(1),'ro', 'markersize',5,'markerfacecolor','r');
-  for i = 1:iN
-    set(ponto,'XData',todosXN(i,1),'YData',todosXN(i,2),'ZData',todosYN(i));
-    %title(sprintf('Iteracao: %d',iN));
+  plot3(todosX(:,1),todosX(:,2),todosY,'--','linewidth',3,'color',[1 0 1]); % linha dos trajetos
+  ponto = plot3(todosX(1,1),todosX(1,2),todosY(1),'-o', 'markerfacecolor','w','markersize',6,'color','w');  % ? erro q acontece error: set: invalid handle (= -37.4789)
+  for k = 1:n
+    set(ponto,'XData',todosX(k,1),'YData',todosX(k,2),'ZData',todosY(k));
+    drawnow;
     pause(2);
   endfor
-
 endfunction
 
-##function plotagrafico3DGradiente(todosXG,todosYG,todosZ,intervaloX,intervaloY,iN)
-##  figure(2);
-##  clf;
-##  surf(intervaloX,intervaloY,todosZ); %plotando o grafico com a superficie
-##  hold on;
-##  grid on;
-##  xlabel('x');
-##  ylabel('y');
-##  zlabel('f(x,y)');
-##  title('Grafico 3D animado da otimizacao da funcao Himmelblau utilizando o metodo do Gradiente ');
-##  colormap jet;
-##
-## endfunction
+ function plotaconvergenciaX(fig,metodo,todosX) % convergencia para X
+    figure(fig);
+    clf;
+    plot(todosX,'-o','markersize',7,'linewidth',3,'markerfacecolor','g');
+    title(sprintf('Grafico de convergencia de X para funcao Himmelblau com o metodo de %s ',metodo),'fontsize',13);
+    xlabel('Iteracoes','fontsize',12);
+    ylabel('f(x,y)','fontsize',12);
+    legend('x','y','fontsize',12)
+    grid on;
+  endfunction
 
+  function plotaconvergenciaY(fig,metodo,todosY)  % convergencia para todosY  = valores para  f(x,y)
+    figure(fig);
+    clf;
+    plot(todosY,'-o','markersize',7,'linewidth',3,'markerfacecolor','g','color','y');
+    title(sprintf('Grafico de convergencia de f(x,y) para funcao Himmelblau com o metodo de %s ',metodo),'fontsize',13);
+    xlabel('Iteracoes','fontsize',12);
+    ylabel('f(x,y)','fontsize',12);
+    legend('Valor encontrado em f(x,y)','fontsize',12)
+    grid on;
+  endfunction
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  function printarInfo(metodo,pontoOtimo,todosX,todosY,i,a)
+    printf('Utilizando o metodo %s com alpha = %.6f, o ponto otimo encontrado foi: (%.6f , %.6f), em %d iteracoes\n',metodo,a,pontoOtimo(1),pontoOtimo(2),i);
+    printf('Distancia do ponto encontrado em relacao ao ponto otimo dado (3,2): %.6f\n', norm( pontoOtimo - [3;2]));
+    printf('Valor de f(x,y) no ponto (%.6f , %.6f) = %.6f\n\n\n',pontoOtimo(1),pontoOtimo(2),todosY(end)); % pegando o valor de f(x,y) da ultima iteracao
+  endfunction
 endfunction
